57807c70cd4f4047e63c3e9efaa0f687eb1d1223
swatisaulanki <swatisaulanki@gmail.com>
2024-07-23 15:28:00 +0530

    code refactor

--- src/components/ProductsManagement/ProductsManageForm.jsx
+++ src/components/ProductsManagement/ProductsManageForm.jsx
@@ -70,6 +70,126 @@ const ProductsManageForm = () => {
           </div>
         )}
 
+
+
+        {selectedProductType === "Gold SIP" && (
+          <div className="form-group">
+            <label htmlFor="GoldSIPOptions" className="label"> Gold SIP Options:</label>
+            <select
+              id="GoldSIPOptions"
+              name="f_GoldSIPOptions"
+              required
+              className="select"
+            >
+              <option value="">Select</option>
+              <option value="Option 1">Daily</option>
+              <option value="Option 1">Weekly</option>
+              <option value="Option 1">Monthly</option>
+
+            </select>
+          </div>
+        )}
+
+
+        {selectedProductType === "FineX" && (
+          <div className="form-group">
+            <label htmlFor="FineX" className="label"> FineX Options:</label>
+            <select
+              id="FineX"
+              name="FineX"
+              required
+              className="select"
+            >
+              <option value="">Select</option>
+              <option value="Option 1">1 Month</option>
+              <option value="Option 1">3 Month</option>
+              <option value="Option 1">6 Month</option>
+              <option value="Option 1">12 Month</option>
+
+            </select>
+          </div>
+        )}
+
+
+
+        {selectedProductType === "FinePlus" && (
+          <div className="form-group">
+            <label htmlFor="FinePlus" className="label"> FinePlus Options:</label>
+            <select
+              id="FinePlus"
+              name="f_FinePlus"
+              required
+              className="select"
+            >
+              <option value="">Select</option>
+              <option value="Option 1">1 yr</option>
+              <option value="Option 1">2 yr</option>
+              <option value="Option 1">3 yr</option>
+              <option value="Option 1">4 yr</option>
+              <option value="Option 1">5 yr</option>
+            </select>
+          </div>
+        )}
+
+
+
+        {selectedProductType === "GoldPro" && (
+          <div className="form-group">
+            <label htmlFor="GoldProOptions" className="label"> Gold Pro Options:</label>
+            <select
+              id="GoldProOptions"
+              name="f_GoldProOptions"
+              required
+              className="select"
+            >
+              <option value="">Select</option>
+              <option value="Option 1">1 Month</option>
+              <option value="Option 1">3 Month</option>
+              <option value="Option 1">6 Month</option>
+              <option value="Option 1">12 Month</option>
+
+            </select>
+          </div>
+        )}
+
+        {selectedProductType === "GoldPlus" && (
+          <div className="form-group">
+            <label htmlFor="GoldPlusOptions" className="label"> Gold Plus Options:</label>
+            <select
+              id="GoldPlusOptions"
+              name="f_GoldPlusOptions"
+              required
+              className="select"
+            >
+              <option value="">Select</option>
+              <option value="Option 1">1 yr</option>
+              <option value="Option 1">2 yr</option>
+              <option value="Option 1">3 yr</option>
+              <option value="Option 1">4 yr</option>
+              <option value="Option 1">5 yr</option>
+
+            </select>
+          </div>
+        )}
+        {selectedProductType === "Merchant" && (
+          <div className="form-group">
+            <label htmlFor="MerchantOptions" className="label"> Merchant Options:</label>
+            <select
+              id="MerchantOptions"
+              name="f_MerchantOptions"
+              required
+              className="select"
+            >
+              <option value="">Select</option>
+              <option value="Daily">Daily</option>
+              <option value="Weekly">Weekly</option>
+              <option value="Monthly">Monthly</option>
+
+            </select>
+          </div>
+        )}
+
+        
         <div className="form-group">
           <label htmlFor="ammount" className="label">Amount:</label>
           <input
